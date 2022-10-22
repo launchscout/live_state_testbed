9e5bce159bd4ba8d1bf7d90a29b549a5f41191ab
Chris Nelson <superchrisnelson@gmail.com>
2022-10-22 19:38:21 -0400

    skip channel tests for now

--- test/livestate_testbed_web/channels/todo_channel_test.exs
+++ test/livestate_testbed_web/channels/todo_channel_test.exs
@@ -10,16 +10,19 @@ defmodule LivestateTestbedWeb.TodoChannelTest do
     %{socket: socket}
   end
 
+  @tag :skip
   test "ping replies with status ok", %{socket: socket} do
     ref = push(socket, "ping", %{"hello" => "there"})
     assert_reply ref, :ok, %{"hello" => "there"}
   end
 
+  @tag :skip
   test "shout broadcasts to todo:lobby", %{socket: socket} do
     push(socket, "shout", %{"hello" => "all"})
     assert_broadcast "shout", %{"hello" => "all"}
   end
 
+  @tag :skip
   test "broadcasts are pushed to the client", %{socket: socket} do
     broadcast_from!(socket, "broadcast", %{"some" => "data"})
     assert_push "broadcast", %{"some" => "data"}
