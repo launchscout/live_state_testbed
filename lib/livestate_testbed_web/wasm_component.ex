2427013be06f4dff47f52e7c17c84a13f406d865
Scott <dscottwiggins@gmail.com>
2023-10-30 14:22:24 -0400

    add elixir todos with javascript using rust

--- lib/livestate_testbed_web/wasm_component.ex
+++ lib/livestate_testbed_web/wasm_component.ex
@@ -2,5 +2,8 @@ defmodule LivestateTestbedWeb.WasmComponent do
   use Rustler, otp_app: :livestate_testbed, crate: "livestatetestbedweb_wasmcomponent"
 
   # When your NIF is loaded, it will override this function.
-  def other_init(), do: :erlang.nif_error(:nif_not_loaded)
+  def other_init(), do: error()
+  def other_add_todo(_todo, _todo_list), do: error()
+
+  defp error, do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/livestatetestbedweb_wasmcomponent/src/lib.rs
+++ native/livestatetestbedweb_wasmcomponent/src/lib.rs
@@ -9,8 +9,6 @@ wasmtime::component::bindgen!({
     world: "todo-list"
 });
 
-rustler::init!("Elixir.LivestateTestbedWeb.WasmComponent", [other_init]);
-
 #[rustler::nif]
 fn other_init() -> Vec<String> {
     let mut config = Config::default();
@@ -37,9 +35,34 @@ fn other_init() -> Vec<String> {
 
     // let new_result = instance
     //     .call_add_todo(&mut store, "more", &result[..])
-    //     .context("Failed to call init function")?;
+    //     .context("Failed to call init function")
+    //     .unwrap();
     
     // println!("Got {} and {} and {}", new_result[0], new_result[1], new_result[2]);
     
-    // Ok(())
+    // new_result
 }
+
+#[rustler::nif]
+fn other_add_todo(todo: String, todo_list: Vec<String>) -> Vec<String> {
+    let mut config = Config::default();
+    config.wasm_component_model(true);
+    let engine = Engine::new(&config).unwrap();
+    let linker = Linker::new(&engine);
+
+    let mut table = Table::new();
+
+    let wasi = WasiCtxBuilder::new()
+        .build(&mut table);
+    let mut store = Store::new(&engine, wasi);
+
+    let component = Component::from_file(&engine, "./todo-list.wasm").context("Component file not found").unwrap();
+
+    let (instance, _) = TodoList::instantiate(&mut store, &component, &linker).unwrap();
+
+    println!("after instantiation, before execution"); 
+
+    instance.call_add_todo(&mut store, &todo, &todo_list[..]).unwrap()
+}
+
+rustler::init!("Elixir.LivestateTestbedWeb.WasmComponent", [other_init, other_add_todo]);
